181dc8c93a39f10b0e07da9f523b121b3e60e200
Davut Çelik <davut.celik2002d@gmail.com>
2024-05-19 11:17:11 +0300

    first commit

---
+++ anasayfa.js
@@ -0,0 +1,29 @@
+// FİRST SLİDER
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 10,
+    slidesPerGroup: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// WİDGET CONTAİNER 1
+var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+    },
+});
+
+// WİDGET CONTAİNER 2
+var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+});
